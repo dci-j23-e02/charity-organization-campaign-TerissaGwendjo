cdea692b9367be6dc79b84dbf6ca18cf8d3e1443
dci-student <Terissa>
2023-11-15 09:55:18 +0100

    Charity Organization (Silver Achievement)

--- main/java/src/CharityManagementSystem.java
+++ main/java/src/CharityManagementSystem.java
@@ -1,16 +1,9 @@
 import java.util.*;
 
 public class CharityManagementSystem {
-    // Using HashMap for Donors with donor's ID as the key
     private static HashMap<Integer, DonorClass> donorsMap = new HashMap<>();
-
-    // Using HashSet for Volunteers
     private static HashSet<VolunteerClass> volunteersSet = new HashSet<>();
-
-    // Using TreeMap for Projects with project's name as the key
     private static TreeMap<String, ProjectClass> projectsMap = new TreeMap<>();
-
-    // Using LinkedList for Donations
     private static LinkedList<DonationClass> donationsList = new LinkedList<>();
 
     public static void main(String[] args) {
@@ -18,72 +11,104 @@ public class CharityManagementSystem {
 
         // Add Donors
         System.out.println("Add Donors:");
-        DonorClass donor1 = addDonor("John Doe", "1234567890", "johndoe@gmail.com");
-        DonorClass donor2 = addDonor("Jane Smith", "0987654321", "janesmith@gmail.com");
+        DonorClass donor1 = addDonor(scanner);
+        DonorClass donor2 = addDonor(scanner);
 
         // Add Volunteers
         System.out.println("\nAdd Volunteers:");
-        VolunteerClass volunteer1 = addVolunteer("Volunteer 1", "111222333", "volunteer1@gmail.com");
-        VolunteerClass volunteer2 = addVolunteer("Volunteer 2", "444555666", "volunteer2@gmail.com");
+        VolunteerClass volunteer1 = addVolunteer(scanner);
+        VolunteerClass volunteer2 = addVolunteer(scanner);
 
         // Add Projects
         System.out.println("\nAdd Projects:");
-        ProjectClass project1 = addProject("Clean Water Project", "Providing clean water to remote areas", 50000);
-        ProjectClass project2 = addProject("Education for All", "Ensuring education access for everyone", 75000);
+        ProjectClass project1 = addProject(scanner);
+        ProjectClass project2 = addProject(scanner);
 
         // Add Donations
         System.out.println("\nAdd Donations:");
-        DonationClass donation1 = addDonation(donor1.getId(), 1000, "2023/11/01", project1.getProjectName());
-        DonationClass donation2 = addDonation(donor2.getId(), 500, "2023/11/02", project2.getProjectName());
+        DonationClass donation1 = addDonation(scanner, donor1.getId(), project1.getProjectName());
+        DonationClass donation2 = addDonation(scanner, donor2.getId(), project2.getProjectName());
 
         // Get and display Donor details
         System.out.println("\nGet Donor Details:");
-        getDonorDetails(1);
+        getDonorDetails(scanner);
 
         // Get and display Volunteer details
         System.out.println("\nGet Volunteer Details:");
-        getVolunteerDetails(1);
+        getVolunteerDetails(scanner);
 
         // Get and display Project details
         System.out.println("\nGet Project Details:");
-        getProjectDetails("Clean Water Project");
+        getProjectDetails(scanner);
 
         // Get and display Donation details
         System.out.println("\nGet Donation Details:");
-        getDonationDetails(1);
+        getDonationDetails(scanner);
 
         scanner.close();
     }
 
-    private static DonorClass addDonor(String name, String contactNumber, String email) {
+    private static DonorClass addDonor(Scanner scanner) {
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Contact Number: ");
+        String contactNumber = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
         DonorClass donor = new DonorClass(name, contactNumber, email);
         donorsMap.put(donor.getId(), donor);
         System.out.println("DonorClass added successfully.");
         return donor;
     }
 
-    private static VolunteerClass addVolunteer(String name, String contactNumber, String email) {
+    private static VolunteerClass addVolunteer(Scanner scanner) {
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Contact Number: ");
+        String contactNumber = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
         VolunteerClass volunteer = new VolunteerClass(name, contactNumber, email);
         volunteersSet.add(volunteer);
         System.out.println("VolunteerClass added successfully.");
         return volunteer;
     }
 
-    private static ProjectClass addProject(String name, String description, double targetAmount) {
-        ProjectClass project = new ProjectClass(name, description, targetAmount);
+    private static ProjectClass addProject(Scanner scanner) {
+        System.out.print("Project Name: ");
+        String projectName = scanner.nextLine();
+        System.out.print("Project Description: ");
+        String projectDescription = scanner.nextLine();
+        System.out.print("Target Amount: ");
+        double targetAmount = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+
+        ProjectClass project = new ProjectClass(projectName, projectDescription, targetAmount);
         projectsMap.put(project.getProjectName(), project);
         System.out.println("ProjectClass added successfully.");
         return project;
     }
 
-    private static DonationClass addDonation(int donorId, double amount, String date, String projectName) {
+    private static DonationClass addDonation(Scanner scanner, int donorId, String projectName) {
+        System.out.print("Amount: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+        System.out.print("Date (YYYY/MM/DD): ");
+        String date = scanner.nextLine();
+
         DonationClass donation = new DonationClass(donorId, amount, date, projectName);
         donationsList.add(donation);
         System.out.println("DonationClass added successfully.");
         return donation;
     }
 
-    private static void getDonorDetails(int donorId) {
+    private static void getDonorDetails(Scanner scanner) {
+        System.out.print("Enter Donor ID: ");
+        int donorId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
         DonorClass donor = donorsMap.get(donorId);
         if (donor != null) {
             System.out.println("DonorClass Details: Name: " + donor.getName() + ", Contact Number: " +
@@ -93,7 +118,11 @@ public class CharityManagementSystem {
         }
     }
 
-    private static void getVolunteerDetails(int volunteerId) {
+    private static void getVolunteerDetails(Scanner scanner) {
+        System.out.print("Enter Volunteer ID: ");
+        int volunteerId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
         VolunteerClass volunteer = findVolunteerById(volunteerId);
         if (volunteer != null) {
             System.out.println("VolunteerClass Details: Name: " + volunteer.getName() + ", Contact Number: " +
@@ -112,7 +141,10 @@ public class CharityManagementSystem {
         return null;
     }
 
-    private static void getProjectDetails(String projectName) {
+    private static void getProjectDetails(Scanner scanner) {
+        System.out.print("Enter Project Name: ");
+        String projectName = scanner.nextLine();
+
         ProjectClass project = projectsMap.get(projectName);
         if (project != null) {
             System.out.println("ProjectClass Details: Description: " + project.getProjectDescription() +
@@ -122,7 +154,11 @@ public class CharityManagementSystem {
         }
     }
 
-    private static void getDonationDetails(int donationId) {
+    private static void getDonationDetails(Scanner scanner) {
+        System.out.print("Enter Donation ID: ");
+        int donationId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
         DonationClass donation = findDonationById(donationId);
         if (donation != null) {
             System.out.println("DonationClass Details: DonorClass ID: " + donation.getDonorId() +
@@ -141,4 +177,6 @@ public class CharityManagementSystem {
         }
         return null;
     }
+
+    //GOLDEN ACHIEVEMENT
 }
